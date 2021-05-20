68e216a93121bb2a6c2d8a636d5d47465bdb1e93
Solleti Kowsalya <70649345+kowsalyaprasad98@users.noreply.github.com>
2021-05-20 08:40:18 +0530

    Update set_count.cpp

--- set_count.cpp
+++ set_count.cpp
@@ -3,14 +3,15 @@ using namespace std;
 int main()
 {
    int k;
-   cin>>k;
+   cin>>k;//10
    int count=0;
+   //logic to count set bits
    while(k!=1)
    {
-   	if(k&1==1)
+   	if(k&1==1)// 1010 & 0001 --->0101 & 0001 --->
     {
    	count++;
-	}
+    }
 	k=k>>1;
    }
    cout<<count;
