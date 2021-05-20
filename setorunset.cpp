00ee7800660fd9bd0700758c620a3df53b432560
Solleti Kowsalya <70649345+kowsalyaprasad98@users.noreply.github.com>
2021-05-20 08:43:49 +0530

    Update setorunset.cpp

--- setorunset.cpp
+++ setorunset.cpp
@@ -4,8 +4,9 @@ int main()
 {
 	int s,pos;
 	cin>>s;
+	// logic to check the position bit is set or not
 	cin>>pos;
-	if(s&((pos-1)<<1)
+	if(s&((pos-1)<<1) // if(s&(1<<(pos-1)>0)
 	{
 		cout<<"yes";
 	}
